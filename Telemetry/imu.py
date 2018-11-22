f4ecb04c258c7ee900bd93cf6cf1cdc4e69b0099
Eric Haengel <ehaengel@apsistech.com>
2018-11-22 08:41:16 +0000

    Moved IMU functionality to class

--- Telemetry/imu.py
+++ Telemetry/imu.py
@@ -1,12 +1,61 @@
 #!/usr/bin/python
+import time
 import smbus
+import ctypes
+from math import *
 
 class InertialMeasurementUnit:
     def __init__(self):
-        pass
+        self.InitializeDevice()
 
     # High level functions
     def Update(self):
-        pass
+        self.UpdateMeasurementData()
+
+    def GetAcceleration(self):
+        return self.accel
+
+    def GetGyrometer(self):
+        return self.gyro
+
+    def GetMagnetometer(self):
+        return self.mag
 
     # Low level functions
+    def InitializeDevice(self):
+        bus = smbus.SMBus(1)
+        bus.write_byte_data(0x69, 0x40, 0b00010101)
+        bus.write_byte_data(0x69, 0x41, 0b00000011)
+        bus.write_byte_data(0x69, 0x43, 0b00000100)
+        bus.write_byte_data(0x69, 0x44, 0b00000011)
+        bus.write_byte_data(0x69, 0x4C, 0b00000000)
+        bus.write_byte_data(0x69, 0x7E, 0x11)
+        bus.write_byte_data(0x69, 0x7E, 0x15)
+        bus.write_byte_data(0x69, 0x7E, 0x19)
+        bus.close()
+
+    def UpdateMeasurementData(self):
+        bus = smbus.SMBus(1)
+        self.raw_measurement_data = bus.read_i2c_block_data(0x69, 0x04, 20)
+        print bin(bus.read_byte_data(0x69, 0x03))
+        print bin(bus.read_byte_data(0x69, 0x1B))
+        bus.close()
+
+        self.accel[2] = round(ctypes.c_short((self.raw_measurement_data[19]<<8) | self.raw_measurement_data[18]).value * 2.0 / 2**15, 2)
+        self.accel[1] = round(ctypes.c_short((self.raw_measurement_data[17]<<8) | self.raw_measurement_data[16]).value * 2.0 / 2**15, 2)
+        self.accel[0] = round(ctypes.c_short((self.raw_measurement_data[15]<<8) | self.raw_measurement_data[14]).value * 2.0 / 2**15, 2)
+
+        self.gyro[2] = round(ctypes.c_short((self.raw_measurement_data[13]<<8) | self.raw_measurement_data[12]).value * 125.0 / 2**15, 2)
+        self.gyro[1] =  round(ctypes.c_short((self.raw_measurement_data[11]<<8) | self.raw_measurement_data[10]).value * 125.0 / 2**15, 2)
+        self.gyro[0] =  round(ctypes.c_short((self.raw_measurement_data[9]<<8) | self.raw_measurement_data[8]).value * 125.0 / 2**15, 2)
+
+        self.mag[2] = ctypes.c_short((self.raw_measurement_data[5]<<8) | self.raw_measurement_data[4]).value
+        self.mag[1] =  ctypes.c_short((self.raw_measurement_data[3]<<8) | self.raw_measurement_data[2]).value
+        self.mag[0] =  ctypes.c_short((self.raw_measurement_data[1]<<8) | self.raw_measurement_data[0]).value
+
+
+    # Register data
+    raw_measurement_data = [ 0x00 ] * 20
+    accel = [ 0.00, 0.00, 0.00 ]
+    gyro = [ 0.00, 0.00, 0.00 ]
+    mag = [ 0.00, 0.00, 0.00 ]
--- Telemetry/main.py
+++ Telemetry/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import rtc
 import environment
+import imu
 import datetime
 import time
 import smbus
@@ -22,12 +23,17 @@ environment_sensor.CalculateTemperature()
 environment_sensor.CalculatePressure()
 environment_sensor.CalculateHumidity()
 
-print "Temperature =", environment_sensor.GetTemperature()
-print "Pressure =", environment_sensor.GetPressure()
-print "Humidity =", environment_sensor.GetHumidity()'''
+print 'Temperature =', environment_sensor.GetTemperature()
+print 'Pressure =', environment_sensor.GetPressure()
+print 'Humidity =', environment_sensor.GetHumidity()'''
 
 # IMU
-bus = smbus.SMBus(1)
+inertial_measurement_unit = imu.InertialMeasurementUnit()
+inertial_measurement_unit.Update()
+print 'Acceleration =', inertial_measurement_unit.GetAcceleration()
+print 'Gyrometer =', inertial_measurement_unit.GetGyrometer()
+print 'Magnetometer =', inertial_measurement_unit.GetMagnetometer()
+'''bus = smbus.SMBus(1)
 print hex(bus.read_byte_data(0x69, 0x00))
 bus.write_byte_data(0x69, 0x40, 0b0010101)
 bus.write_byte_data(0x69, 0x41, 0b0000011)
@@ -38,4 +44,4 @@ raw_measurement_data = bus.read_i2c_block_data(0x69, 0x04, 20)
 print ctypes.c_short((raw_measurement_data[19]<<8) | raw_measurement_data[18]).value * 2.0 / 2**15
 print ctypes.c_short((raw_measurement_data[17]<<8) | raw_measurement_data[16]).value * 2.0 / 2**15
 print ctypes.c_short((raw_measurement_data[15]<<8) | raw_measurement_data[14]).value * 2.0 / 2**15
-bus.close()
+bus.close()'''
